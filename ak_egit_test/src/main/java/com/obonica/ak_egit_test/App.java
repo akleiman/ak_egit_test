b0d5d50e87cfd0440edf693cb94b8dbcad7068fb
Alan Kleiman <kleiman@obonica.com>
2012-07-16 00:36:47 -0300

    First commit.

---
+++ ak_egit_test/src/main/java/com/obonica/ak_egit_test/App.java
@@ -0,0 +1,13 @@
+package com.obonica.ak_egit_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
